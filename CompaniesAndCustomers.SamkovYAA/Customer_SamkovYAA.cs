cc381818bf155968f4093b884c5fc7a811220565
Yaroslav Samkov <kotyaruii@gmail.com>
2024-06-21 20:44:17 +0500

    Добавлены все необходимые классы в библиотеку. Создано приложение ВПФ и модульный тест. Наполнен код в кастомер по примеру из видео.

--- CompaniesAndCustomers.SamkovYAA/Customer_SamkovYAA.cs
+++ CompaniesAndCustomers.SamkovYAA/Customer_SamkovYAA.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LibCompAndCust.SamkovYAA
 {
-    internal class Customer_SamkovYAA
+    public class Customer_SamkovYAA
     {
+        public int ID { get; set; }
+        
+        public string Name { get; set; }
+        
+        public int CompanyID{ get; set; }
 
+        public Company Company { get; set; }
+
+        public Customer_SamkovYAA() { }
+
+        public Customer_SamkovYAA(string name, int companyID)
+        {
+            this.Name = name;
+            this.CompanyID = companyID;
+        }
+
+        public Customer_SamkovYAA(string name, Company company)
+        {
+            this.Name = name;
+            this.Company = company;
+            this.CompanyID = company.ID;
+        }
     }
 }
---
+++ TestsForLibCompAndCust.SamkovYAA/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsForLibCompAndCust.SamkovYAA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsForLibCompAndCust.SamkovYAA")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("784efa8c-93d2-4865-90e4-ef696a301528")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
