84b15fb209dc183f34d4cba66e1ac4460b24255b
Yaroslav Samkov <kotyaruii@gmail.com>
2024-06-23 01:33:20 +0500

    Все что было можно скопировано и начинаются проблемы. Нужно написать код всплывающего окна через которое идёт добавление/изменение/удаление комании/сотрудника.

--- WpfAppCompAndCust.SamkovYAA/CommandWindow.xaml.cs
+++ WpfAppCompAndCust.SamkovYAA/CommandWindow.xaml.cs
@@ -23,5 +23,15 @@ namespace WpfAppCompAndCust.SamkovYAA
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
